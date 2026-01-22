0660dd80221043a98368fb40fe766111790306fc
HemuDabas <hemudabas22@gmail.com>
2026-01-22 12:15:00 +0530

    output export

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
  "devIndicators" :false ,
  
+  output: 'export', // for static export
+  images: {
+    unoptimized: true, // required if you use next/image
+  },
 };
 
 export default nextConfig;
