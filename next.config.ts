9241288abf74be046c7046a3b9ad82217a0ab425
HemuDabas <hemudabas22@gmail.com>
2026-01-22 12:28:15 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
  "devIndicators" :false ,
  reactStrictMode: true,
  
-  output: 'export', // for static export
+ 
   images: {
     unoptimized: true, // required if you use next/image
   },
